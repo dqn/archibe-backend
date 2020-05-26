e5dc6e21305a880f03e4c02e696d91ddb5a01218
dqn <suyarizzz@gmail.com>
2020-05-26 22:23:22 +0900

    fix: index

--- sqls/indexes.sql
+++ sqls/indexes.sql
@@ -1,11 +1,11 @@
 DROP INDEX IF EXISTS uq_channels_channel_id CASCADE;
-DROP INDEX IF EXISTS uq_videos_channel_id CASCADE;
 DROP INDEX IF EXISTS uq_videos_video_id CASCADE;
-DROP INDEX IF EXISTS uq_chats_channel_id CASCADE;
-DROP INDEX IF EXISTS uq_chats_video_id CASCADE;
+DROP INDEX IF EXISTS idx_videos_channel_id CASCADE;
+DROP INDEX IF EXISTS idx_chats_video_id CASCADE;
+DROP INDEX IF EXISTS idx_chats_channel_id CASCADE;
 
 CREATE UNIQUE INDEX uq_channels_channel_id ON channels(channel_id);
-CREATE UNIQUE INDEX uq_videos_channel_id ON videos(channel_id);
 CREATE UNIQUE INDEX uq_videos_video_id ON videos(video_id);
-CREATE UNIQUE INDEX uq_chats_channel_id ON chats(channel_id);
-CREATE UNIQUE INDEX uq_chats_video_id ON chats(video_id);
+CREATE INDEX idx_videos_channel_id ON videos(channel_id);
+CREATE INDEX idx_chats_video_id ON chats(video_id);
+CREATE INDEX idx_chats_channel_id ON chats(channel_id);

b0c8e4e764c68db1e15737e86ffc0ab8b6fd6f52
dqn <suyarizzz@gmail.com>
2020-06-07 18:13:43 +0900

    feat: fetch and insert more video info

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/lib/pq v1.5.2
 	google.golang.org/appengine v1.6.6 // indirect
+	github.com/dqn/ytvi v0.0.0-20200606201819-626a010ddfd8
 )
--- scripts/insert/main.go
+++ scripts/insert/main.go
@@ -5,11 +5,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/dqn/chatlog"
 	"github.com/dqn/chatlog/chat"
 	"github.com/dqn/tubekids/dbexec"
 	"github.com/dqn/tubekids/models"
+	"github.com/dqn/ytvi"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -74,13 +76,12 @@ func processEachChatItem(cl *chatlog.Chatlog, handler func(item *chat.ChatItem) 
 }
 
 func run() error {
-	if len(os.Args) != 4 {
+	if len(os.Args) != 3 {
 		return fmt.Errorf("invalid arguments")
 	}
 
 	dsn := os.Args[1]
-	channelID := os.Args[2]
-	videoID := os.Args[3]
+	videoID := os.Args[2]
 
 	cl, err := chatlog.New(videoID)
 	if err != nil {
@@ -203,10 +204,59 @@ func run() error {
 	defer db.Close()
 	dbx := dbexec.NewExecutor(db)
 
-	_, err = dbx.Videos.InsertOne(&models.Video{
-		VideoID:   videoID,
-		ChannelID: channelID,
-	})
+	pr, err := ytvi.GetVideoInfo(videoID)
+	if err != nil {
+		return err
+	}
+
+	pmr := pr.Microformat.PlayerMicroformatRenderer
+
+	ownerChannel := models.Channel{
+		ChannelID: pmr.ExternalChannelID,
+		Name:      pmr.OwnerChannelName,
+		ImageURL:  pmr.OwnerProfileURL,
+	}
+
+	channels = append(channels, ownerChannel)
+
+	lengthSeconds, err := strconv.ParseInt(pmr.LengthSeconds, 10, 64)
+	if err != nil {
+		return err
+	}
+	viewCount, err := strconv.ParseInt(pmr.ViewCount, 10, 64)
+	if err != nil {
+		return err
+	}
+	publishDate, err := time.Parse("2006-01-02", pmr.PublishDate)
+	if err != nil {
+		return err
+	}
+	uploadDate, err := time.Parse("2006-01-02", pmr.UploadDate)
+	if err != nil {
+		return err
+	}
+
+	thumbnails := pr.VideoDetails.Thumbnail.Thumbnails
+	thumbnailURL := thumbnails[len(thumbnails)-1].URL
+
+	video := models.Video{
+		VideoID:       videoID,
+		ChannelID:     pmr.ExternalChannelID,
+		Title:         pmr.Title.SimpleText,
+		Description:   pmr.Description.SimpleText,
+		LengthSeconds: lengthSeconds,
+		ViewCount:     viewCount,
+		AverageRating: pr.VideoDetails.AverageRating,
+		ThumbnailURL:  thumbnailURL,
+		Category:      pmr.Category,
+		IsPrivate:     pr.VideoDetails.IsPrivate,
+		PublishDate:   &publishDate,
+		UploadDate:    &uploadDate,
+		LiveStartedAt: &pmr.LiveBroadcastDetails.StartTimestamp,
+		LiveEndedAt:   &pmr.LiveBroadcastDetails.EndTimestamp,
+	}
+
+	_, err = dbx.Videos.InsertOne(&video)
 	if err != nil {
 		return err
 	}

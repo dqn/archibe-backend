c151e9702325a0cb2e6e3c7eab25355d392d6b56
dqn <suyarizzz@gmail.com>
2020-06-08 17:42:31 +0900

    test: add videos test

---
+++ dbexec/video_test.go
@@ -0,0 +1,58 @@
+package dbexec
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/dqn/tubekids/models"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func TestVideosInsertOne(t *testing.T) {
+	dsn := os.Getenv("DSN")
+
+	db, err := sqlx.Open("postgres", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ex := VideosExecutor{db}
+
+	now := time.Now()
+	_, err = ex.InsertOne(&models.Video{
+		VideoID:       "AAA",
+		ChannelID:     "BBB",
+		Title:         "CCC",
+		Description:   "DDD",
+		LengthSeconds: 42,
+		ViewCount:     42,
+		AverageRating: 3.14,
+		ThumbnailURL:  "EEE",
+		Category:      "FFF",
+		IsPrivate:     false,
+		PublishDate:   &now,
+		UploadDate:    &now,
+		LiveStartedAt: &now,
+		LiveEndedAt:   &now,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestVideosFind(t *testing.T) {
+	dsn := os.Getenv("DSN")
+
+	db, err := sqlx.Open("postgres", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ex := VideosExecutor{db}
+
+	_, err = ex.Find("VIDEO_A")
+	if err != nil {
+		t.Fatal(err)
+	}
+}

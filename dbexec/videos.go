61eb880b3202496eab037b48dde0ac54a84279a2
dqn <suyarizzz@gmail.com>
2020-06-09 23:18:08 +0900

    feat: add total purchase amounts and channel to video

--- dbexec/videos.go
+++ dbexec/videos.go
@@ -87,9 +87,38 @@ func (e *VideosExecutor) Find(videoID string) (*models.Video, error) {
 		t1.live_started_at,
 		t1.live_ended_at,
 		t1.created_at,
-		t1.updated_at
+		t1.updated_at,
+		(
+			SELECT
+				COALESCE(jsonb_agg(u1.object), '[]')
+			FROM (
+				SELECT
+					jsonb_build_object(
+						'currency_unit',
+						v1.currency_unit,
+						'total_purchase_amount',
+						SUM(v1.purchase_amount)
+					) AS object
+				FROM
+					chats AS v1
+				WHERE
+					v1.video_id = $1
+					AND v1.currency_unit != ''
+				GROUP BY
+					v1.currency_unit
+			) AS u1
+		) AS total_purchase_amounts,
+		t2.id AS "channel.id",
+		t2.channel_id AS "channel.channel_id",
+		t2.name AS "channel.name",
+		t2.image_url AS "channel.image_url",
+		t2.created_at AS "channel.created_at",
+		t2.updated_at AS "channel.updated_at"
 	FROM
 		videos AS t1
+		INNER JOIN channels AS t2 ON (
+			t1.channel_id = t2.channel_id
+		)
 	WHERE
 		video_id = $1
 	`
--- models/video.go
+++ models/video.go
@@ -3,23 +3,36 @@ package models
 import "time"
 
 type Video struct {
-	ID            int64      `db:"id" json:"id"`
-	VideoID       string     `db:"video_id" json:"video_id"`
-	ChannelID     string     `db:"channel_id" json:"channel_id"`
-	Title         string     `db:"title" json:"title"`
-	Description   string     `db:"description" json:"description"`
-	LengthSeconds int64      `db:"length_seconds" json:"length_seconds"`
-	ViewCount     int64      `db:"view_count" json:"view_count"`
-	AverageRating float64    `db:"average_rating" json:"average_rating"`
-	ThumbnailURL  string     `db:"thumbnail_url" json:"thumbnail_url"`
-	Category      string     `db:"category" json:"category"`
-	IsPrivate     bool       `db:"is_private" json:"is_private"`
-	PublishDate   *time.Time `db:"publish_date" json:"publish_date"`
-	UploadDate    *time.Time `db:"upload_date" json:"upload_date"`
-	LiveStartedAt *time.Time `db:"live_started_at" json:"live_started_at"`
-	LiveEndedAt   *time.Time `db:"live_ended_at" json:"live_ended_at"`
-	CreatedAt     *time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt     *time.Time `db:"updated_at" json:"updated_at"`
+	ID                   int64                    `db:"id" json:"id"`
+	VideoID              string                   `db:"video_id" json:"video_id"`
+	ChannelID            string                   `db:"channel_id" json:"channel_id"`
+	Title                string                   `db:"title" json:"title"`
+	Description          string                   `db:"description" json:"description"`
+	LengthSeconds        int64                    `db:"length_seconds" json:"length_seconds"`
+	ViewCount            int64                    `db:"view_count" json:"view_count"`
+	AverageRating        float64                  `db:"average_rating" json:"average_rating"`
+	ThumbnailURL         string                   `db:"thumbnail_url" json:"thumbnail_url"`
+	Category             string                   `db:"category" json:"category"`
+	IsPrivate            bool                     `db:"is_private" json:"is_private"`
+	PublishDate          *time.Time               `db:"publish_date" json:"publish_date"`
+	UploadDate           *time.Time               `db:"upload_date" json:"upload_date"`
+	LiveStartedAt        *time.Time               `db:"live_started_at" json:"live_started_at"`
+	LiveEndedAt          *time.Time               `db:"live_ended_at" json:"live_ended_at"`
+	CreatedAt            *time.Time               `db:"created_at" json:"created_at"`
+	UpdatedAt            *time.Time               `db:"updated_at" json:"updated_at"`
+	TotalPurchaseAmounts TotalPurchaseAmountSlice `db:"total_purchase_amounts" json:"total_purchase_amounts"`
+	Channel              Channel                  `db:"channel" json:"channel"`
+}
+
+type TotalPurchaseAmount struct {
+	CurrencyUnit        string  `json:"currency_unit"`
+	TotalPurchaseAmount float64 `json:"total_purchase_amount"`
+}
+
+type TotalPurchaseAmountSlice []TotalPurchaseAmount
+
+func (t *TotalPurchaseAmountSlice) Scan(val interface{}) error {
+	return scanJSON(t, val)
 }
 
 type VideoSlice []Video
